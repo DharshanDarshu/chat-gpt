b70ec30837b0f9210343eb176393c95dd3d236de
Dharshan Kumar A S <dharshankumar.as@zensar.com>
2023-02-08 20:52:02 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import { SessionProvider } from "../components/SessionProvider";
+import Sidebar from "../components/Sidebar";
+import { getServerSession } from "next-auth";
+import "../styles/globals.css";
+import Login from "../components/Login";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getServerSession(authOptions);
+  return (
+    <html>
+      <head />
+      <body>
+        <SessionProvider session={session}>
+          {session ? (
+            <Login />
+          ) : (
+            <div className='flex'>
+              <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
+                <Sidebar />
+              </div>
+
+              {/* ClientProvider - Notification */}
+              {/* React Hot Notifications */}
+              <div className='bg-[#343541] flex-1'>
+                {children}
+              </div>
+            </div>
+          )}
+        </SessionProvider>
+      </body>
+    </html>
+  );
+}
